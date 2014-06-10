22a0c408fa3351b0365e6f973b7ee1ca2138bfd2
Your Name <you@example.com>
2014-06-10 10:29:31 +0800

    init

---
+++ main.go
@@ -0,0 +1,198 @@
+package main
+
+import(
+    "fmt"
+    "os"
+    "path/filepath"
+    "bufio"
+    "io"
+    "time"
+    "strings"
+    "github.com/howeyc/fsnotify"
+    "runtime"
+   M "fsnotice/models"
+)
+
+var (
+eventTime   = make(map[string]int64)
+buildPeriod time.Time
+DirPath string
+)
+
+func init(){
+    DirPath = M.Cfg.MustValue("base", "path")
+}
+
+func main(){
+    exit := make(chan bool)
+
+    //DirPath = "./TBlist"
+    initData(DirPath)
+    NewWatcher(DirPath)
+
+    for {
+      select{
+        case <-exit:
+            runtime.Goexit()
+      }
+    }
+
+}
+
+func initData(dirPath string){
+    filepath.Walk(dirPath, func(path string, f os.FileInfo, err error) error {
+        if f == nil {
+            return err
+        }
+
+        if f.IsDir(){
+            return nil
+        }
+
+        //path = strings.ToLower(path)
+        if !strings.HasSuffix(path, "TXT"){
+            return nil
+        }
+        fmt.Println(path)
+
+        Save2Mysql(path)
+
+        return nil
+    })
+}
+
+func NewWatcher(path string){
+    watcher, err := fsnotify.NewWatcher()
+    if err != nil {
+        fmt.Println("[ERRO] NewWatcher Failed")
+        os.Exit(2)
+    }
+
+
+    go func(){
+        for {
+            select{
+              case e := <-watcher.Event:
+                  // Prevent duplicated builds.
+				if buildPeriod.Add(1 * time.Second).After(time.Now()) {
+					continue
+				}
+				buildPeriod = time.Now()
+
+				mt := getFileModTime(e.Name)
+				if t := eventTime[e.Name]; mt == t {
+					Flog("[SKIP] # %s #\n", e.String())
+                    continue
+				}
+                eventTime[e.Name] = mt
+                  /*
+                  if e.IsCreate() {
+                      Flog("创建文件：%s", e.Name)
+                  }else if e.IsDelete() {
+                      Flog("删除文件：%s", e.Name)
+                  }else if e.IsModify() {
+                      Flog("修改文件：%s", e.Name)
+                  }else if e.IsRename() {
+                      Flog("重命名文件：%s", e.Name)
+                      */
+                  if e.IsAttrib() {
+                      Save2Mysql(e.Name)
+                      Flog("修改文件属性：%s", e.Name)
+                  }
+              case err := <-watcher.Error:
+                  Flog("err: %s", err.Error())
+            }
+        }
+    }()
+
+    err = watcher.Watch(path)
+    if err != nil {
+
+        Flog("err : fail to watch dir ", err)
+        os.Exit(2)
+    }
+
+
+}
+
+func Flog(msg string, args... interface{}){
+    fmt.Println(msg, args)
+}
+
+func Save2Mysql(file string){
+    f, err := os.Open(file)
+    if err != nil {
+        Flog("[ERRO] 文件读取失败:", err.Error())
+        return
+    }
+    defer f.Close()
+
+    bufreader := bufio.NewReader(f)
+
+    count := 0
+    for{
+        line, err := bufreader.ReadString('\n')
+
+        if err == io.EOF{
+            Flog("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+            break
+        }
+        count ++
+
+        line = strings.Replace(line, "\r\n", "", -1)
+        fmt.Println("读取行：", count)
+        info := make(map[string]string)
+
+        kvs := strings.Split(line, ";")
+        for i := 0; i< len(kvs); i++{
+            kv := strings.Split(kvs[i], "=")
+            if len(kv) != 2 {
+                Flog("[ERRO] 行中键值对格式不正确")
+                continue
+            }
+
+            info[kv[0]] = kv[1]
+
+        }
+
+        fmt.Println(info)
+
+        err = M.SaveTbRecord(info)
+        if err != nil {
+            Flog("[ERRO] 写入数据库失败", err, info)
+            return
+            continue 
+        }
+        
+        fmt.Println("SetStats")
+        err = M.SetStats(fmt.Sprintf("%s_%s", info["FormulaName"], info["Symbol"]))
+        if err != nil {
+            Flog("[ERRO]:写入stat失败 ", err)
+            return
+        }
+
+        fmt.Println(3)
+
+//        Flog("[INFO]写入数据成功", info)
+    }
+}
+
+// getFileModTime retuens unix timestamp of `os.File.ModTime` by given path.
+func getFileModTime(path string) int64 {
+	path = strings.Replace(path, "\\", "/", -1)
+	f, err := os.Open(path)
+	if err != nil {
+		Flog("[ERRO] Fail to open file[ %s ]\n", err)
+		return time.Now().Unix()
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		Flog("[ERRO] Fail to get file information[ %s ]\n", err)
+		return time.Now().Unix()
+	}
+
+	return fi.ModTime().Unix()
+}
+
---
+++ models/record.go
@@ -0,0 +1,143 @@
+package models
+
+import(
+    "time"
+    "errors"
+    "fmt"
+//    "math"
+    "strconv"
+//    "fsnotice/util"
+)
+
+type TbRecord struct{
+    Id string
+    FormulaName string
+    Symbol string
+    Date string
+    Time string
+    Action string
+    Number int32
+    Price float64
+    EntryPrice float64
+    NowPosition int32
+    Profit float64
+    BarNum int32
+    IsProfit int
+}
+
+func SaveTbRecord(info map[string]string) (err error){
+    //tb_record
+
+    v, ok := info["FormulaName"]
+    if !ok {
+        return errors.New("[ERROR]field name FormulaName no exists!")
+    }
+    formula_name := v
+
+    v, ok = info["Symbol"]
+    if !ok {
+        return errors.New("[ERROR]field name Symbol no exists!")
+    }
+    symbol := v
+
+    v, ok = info["date"]
+    if !ok {
+        return errors.New("[ERROR]field name date no exists!")
+    }
+    dateTime, err := time.Parse("20060102", v)
+    if err != nil {
+        return errors.New("[ERROR]field name date parse failed !")
+    }
+    dateStr := dateTime.Format("2006-01-02")
+
+     v, ok = info["time"]
+    if !ok {
+        return errors.New("[ERROR]field name time no exists!")
+    }
+    timeTime, err := time.Parse("0.150405", v)
+    if err != nil {
+        return errors.New("[ERROR]field name time parse failed !")
+    }
+    timeStr := timeTime.Format("15:04:05")
+
+    v, ok = info["action"]
+    if !ok {
+        return errors.New("[ERROR]field name action no exists!")
+    }
+    action := v
+
+    v, ok = info["number"]
+    if !ok {
+        return errors.New("[ERROR]field name number no exists!")
+    }
+    number, _ := strconv.Atoi(v)
+
+    v, ok = info["price"]
+    if !ok {
+        return errors.New("[ERROR]field name price no exists!")
+    }
+    price,_ := strconv.ParseFloat(v, 64)
+
+    v, ok = info["EntryPrice"]
+    if !ok {
+        return errors.New("[ERROR]field named EntryPrice no exists!")
+    }
+    entry_price,_ := strconv.ParseFloat(v, 64)
+
+    v, ok = info["nowPosition"]
+    if !ok {
+        return errors.New("[ERROR]field named nowPosition no exists!")
+    }
+    now_position, _ := strconv.Atoi(v)
+
+    v, ok = info["BarNum"]
+    if !ok {
+        return errors.New("[ERROR]field named BarNum no exists!")
+    }
+    fmt.Println("xxxcccc", v)
+    bar_num, err := strconv.ParseFloat(v, 64)
+    fmt.Println("xxxxx", bar_num, err)
+
+    id := fmt.Sprintf("%s_%s_%s_%s_%s", formula_name, symbol, dateStr, timeStr, action)
+
+    ex := &TbRecord{Id:id}
+    has , err := Engine.Get(ex)
+    if has {
+        fmt.Println(err)
+        return nil
+    }
+
+    profit := 0.00
+    fmt.Println(action, action == "sell")
+
+    // 算出利润
+    if action == "sell" {
+        profit =  (price - entry_price)*float64(number)
+    } else if action == "buytocover" {
+        profit = (entry_price - price) * float64(number)
+    }
+
+    fmt.Println(profit)
+
+    var isProfit int = 0
+    if profit > 0 {
+        isProfit = 3
+    } else if profit < 0 {
+        isProfit = 1
+    } else {
+        isProfit = 2
+    }
+
+//    del := "DELETE FROM `tb_record` WHERE `formula_name`=?, `symbol`=?, `date`=?, `time`=?, `action`=?, `number`=?, `price`=?, `entry_price`=?, `now_position`=?"
+//    Engine.Exec(del, args["formula_name"], args["symbol"], args["date"], args["time"], args["action"], args["number"], args["price"], args["entry_price"], args["now_position"])
+
+
+    sql := "REPLACE INTO `tb_record`(`id`, `formula_name`, `symbol`, `date`, `time`, `action`, `number`, `price`, `entry_price`, `now_position`, `profit`, `is_profit`, `bar_num`) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    _, err = Engine.Exec(sql, id, formula_name, symbol, dateStr, timeStr, action, number, price, entry_price, now_position, profit, isProfit, bar_num)
+
+    if err != nil {
+        return
+    }
+
+    return 
+}
